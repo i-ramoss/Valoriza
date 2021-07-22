f3f4aefe14c5ae9951e81fddb373b5138fd52b25
Ian Ramos <ianramossantos@hotmail.com>
2021-07-22 17:47:58 -0300

    chore: separate routes from server

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,15 @@
 import 'reflect-metadata';
 import express from 'express';
 
+import { router } from './routes';
+
 import './database';
 
 const PORT = 3333;
 
 const app = express();
+
+app.use(express.json());
+app.use(router);
 
 app.listen(PORT, () => console.log(`🔥 Server is running at port ${PORT}`));
