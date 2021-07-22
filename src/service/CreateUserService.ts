316c3ba67cb9df4cec1d81313435c9aed93a74fb
Ian Ramos <ianramossantos@hotmail.com>
2021-07-22 17:48:31 -0300

    feat: create user creation service

---
+++ src/service/CreateUserService.ts
@@ -0,0 +1,33 @@
+import { getCustomRepository } from 'typeorm';
+import { UsersRepository } from '../repositories/UsersRepository';
+
+interface IUserRequest {
+  name: string;
+  email: string;
+  admin?: boolean;
+}
+
+
+class CreateUserService {
+  async execute({ name, email, admin }: IUserRequest) {
+    const usersRepository = getCustomRepository(UsersRepository);
+
+    if (!email) throw new Error('Incorrect email');
+
+    const userAlreadyExists = await usersRepository.findOne({ email });
+
+    if (userAlreadyExists) throw new Error('User already exists');
+
+    const user = usersRepository.create({
+      name,
+      email,
+      admin,
+    });
+
+    await usersRepository.save(user);
+
+    return user;
+  }
+}
+
+export { CreateUserService };
