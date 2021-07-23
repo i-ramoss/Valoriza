b5b36034e57270632369eea73963d8e93c565958
Ian Ramos <ianramossantos@hotmail.com>
2021-07-23 12:25:13 -0300

    chore: create tag service

---
+++ src/service/CreateTagService.ts
@@ -0,0 +1,22 @@
+import { getCustomRepository } from 'typeorm';
+import { TagsRepository } from '../repositories/TagsRepository';
+
+class CreateTagService {
+  async execute(name: string) {
+    const tagsRepositories = getCustomRepository(TagsRepository);
+
+    if (!name) throw new Error('Incorrect name');
+
+    const tagAlreadyExists = await tagsRepositories.findOne({ name });
+
+    if (tagAlreadyExists) throw new Error('Tag already exists');
+
+    const tag = tagsRepositories.create({ name });
+
+    await tagsRepositories.save(tag);
+
+    return tag;
+  }
+}
+
+export { CreateTagService };
