2384ebe23dcc52d3fbf6b4905f78bdf775217ddd
Ian Ramos <ianramossantos@hotmail.com>
2021-07-23 18:06:47 -0300

    feat: add jwt to the project

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 
+import { AuthenticateUserController } from './controllers/AuthenticateUserController';
 import { CreateTagController } from './controllers/CreateTagController';
 import { CreateUserController } from './controllers/CreateUserController';
 import { ensureAdmin } from './middlewares/ensureAdmin';
@@ -8,8 +9,10 @@ const router = Router();
 
 const createUserController = new CreateUserController();
 const createTagController = new CreateTagController();
+const authenticateUserController = new AuthenticateUserController();
 
 router.post('/users', createUserController.handle);
 router.post('/tags', ensureAdmin, createTagController.handle);
+router.post('/login', authenticateUserController.handle);
 
 export { router };
