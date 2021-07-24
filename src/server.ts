6fa69284a545dc22b0d1426e477713dc9c7f80fe
Ian Ramos <ianramossantos@hotmail.com>
2021-07-24 11:40:58 -0300

    chore: add cors

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import 'reflect-metadata';
+import cors from 'cors';
 import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
 
@@ -10,6 +11,7 @@ const PORT = 3333;
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(router);
 
